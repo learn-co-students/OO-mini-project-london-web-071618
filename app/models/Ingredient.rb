f22b7ab31699e23bee44a1289ea39969a173f49d
Adham <admuaq@gmail.com>
2018-07-26 15:42:09 +0100

    To merge

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -13,6 +13,13 @@ class Ingredient
   end
 
   def self.most_common_allergen #adham
+
+  string_array = Allergen.all.map {|x| x.ingredient}
+  freq = string_array.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+  string_array.max_by { |v| freq[v]}
+
+  #Allergen.all.max_by {|obj| obj.name}
+  #RecipeCard.all.max_by {|obj| obj.recipe.name}.recipe
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -11,12 +11,16 @@ oil = Ingredient.new("oil")
 rice = Ingredient.new("rice")
 potatoes = Ingredient.new("potatoes")
 meat = Ingredient.new("meat")
+fish = Ingredient.new("fish")
 
 
 
 ingreds = [oil, rice]
 pie_ingreds = [meat, potatoes]
+sushi_ingreds = [fish, oil]
 fried_rice.add_ingredients(ingreds)
+sushi.add_ingredients(sushi_ingreds)
+cottage_pie.add_ingredients(pie_ingreds)
 adham = User.new("adham")
 ritz = User.new("ritz")
 adham.add_recipe_card(fried_rice, "friday", 5)
@@ -29,9 +33,11 @@ ritz.add_recipe_card(sushi, "sunday", 5)
 ritz.add_recipe_card(sushi, "sunday", 5)
 ritz.add_recipe_card(sushi, "sunday", 5)
 ritz.add_recipe_card(sushi, "sunday", 5)
-ritz.declare_allergen("grapes")
-adham.declare_allergen("peanuts")
-adham.declare_allergen("strawberries")
+ritz.declare_allergen(oil)
+adham.declare_allergen(rice)
+adham.declare_allergen(potatoes)
+ritz.declare_allergen(fish)
+ritz.declare_allergen(potatoes)
 adham.top_three_recipes
 
 binding.pry
